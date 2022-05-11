74694c76605203895ec824efcf40e01d4686ab5a
anastasiia_bogdanova <an87bogdanova@gmail.com>
2022-05-11 23:28:20 +0300

    int

--- PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,8 +1,8 @@
 public class Container {
-    private Integer count;
+    private int count;
 
     public void addCount(int value) {
-        this.count = value;
+         count = count + value;
     }
 
     public int getCount() {
