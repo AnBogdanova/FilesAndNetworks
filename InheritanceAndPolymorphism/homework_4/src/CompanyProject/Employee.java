65bc5e9a2c7fe82313416ecade3790813ca7417b
Max Vodolazhsky <maximazerr@mail.ru>
2022-06-19 22:19:59 +0300

    delete wrong homework_4

--- InheritanceAndPolymorphism/homework_4/src/CompanyProject/Employee.java
+++
@@ -1,5 +0,0 @@
-package CompanyProject;
-
-public interface Employee {
-    int getMonthSalary();
-}
--- InheritanceAndPolymorphism/homework_4/src/CompanyProject/Operator.java
+++
@@ -1,9 +0,0 @@
-package CompanyProject;
-
-public class Operator implements Employee {
-    private static final int FIXED_SALARY = 80000;
-    @Override
-    public int getMonthSalary() {
-        return FIXED_SALARY;
-    }
-}
