8be66eb7920af5017e0119066dc7b9298ee070e8
anastasiia_bogdanova <an87bogdanova@gmail.com>
2022-06-19 17:45:52 +0300

    Company

---
+++ InheritanceAndPolymorphism/homework_4v2/src/main/java/Operator.java
@@ -0,0 +1,7 @@
+public class Operator implements Employee {
+    private static final int FIXED_SALARY = 80000;
+    @Override
+    public int getMonthSalary() {
+        return FIXED_SALARY;
+    }
+}
