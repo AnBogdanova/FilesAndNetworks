732b89e9f18e22b90b069d64afb4e1582875155a
anastasiia_bogdanova <an87bogdanova@gmail.com>
2022-07-02 16:54:33 +0300

    Тест добавления корректных данных Customer

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,7 @@
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
@@ -15,8 +17,15 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
-        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        if(components.length!=4) {
+            throw new ArrayIndexOutOfBoundsException("Неверное количество элементов");
+        } else if (!Pattern.matches(("[^|\\w](\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*)"), components[INDEX_EMAIL].trim()) ||
+                !Pattern.matches(("^7[0-9]{10}"), components[INDEX_PHONE].trim())) {
+            throw new IllegalStateException("Неверный формат");
+        } else {
+            String name = components[INDEX_NAME].trim() + " " + components[INDEX_SURNAME].trim();
+            storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        }
     }
 
     public void listCustomers() {
